edf39ef6fc6e2a413a98b580496995d997dd1eea
juliasilge <julia.silge@gmail.com>
2016-12-21 11:04:41 -0700

    Fix title for theme_roboto

--- R/graphing.R
+++ R/graphing.R
@@ -1,6 +1,7 @@
 #' Minimal ggplot2 theme using the Roboto Condensed and Roboto Bold fonts
 #'
-#' @param ... Arguments passed to \code{theme_minimal}
+#' @param base_size base font size
+#' @param ... Other arguments passed to \code{theme_minimal}
 #'
 #' @details The Roboto Condensed and Roboto Bold fonts are both Google fonts;
 #' they can be found at \url{https://fonts.google.com/specimen/Roboto+Condensed}
@@ -12,11 +13,17 @@
 #'
 #' ggplot(mtcars, aes(wt, mpg)) +
 #'     geom_point() +
+#'     labs(title = "A Lovely Plot",
+#'          subtitle = "What can the subtitle tell us?") +
 #'     theme_roboto()
 #'
 #' @export
-theme_roboto <- function(...) {
+theme_roboto <- function(base_size = 11, ...) {
     ret <- theme_minimal(base_family = "RobotoCondensed-Regular", ...)
-    ret$plot.title <- ggplot2::element_text(family="Roboto-Bold")
+    ret$plot.title <- ggplot2::element_text(family="Roboto-Bold",
+                                            size = rel(1.2),
+                                            hjust = 0, vjust = 1,
+                                            margin = margin(b = base_size / 2 * 1.2)
+                                            )
     ret
 }
