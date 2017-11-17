aff71535ad204df1081bce8e91523c34c944f3af
Julia Silge <julia.silge@gmail.com>
2017-11-17 16:20:46 -0700

    Fix up theme_roboto, add theme_plex

--- R/graphing.R
+++ R/graphing.R
@@ -20,13 +20,79 @@
 #'}
 #'
 #' @export
-theme_roboto <- function(base_size = 11, ...) {
+theme_roboto <- function(base_size = 11,
+                         strip_text_size = 12,
+                         strip_text_margin = 5,
+                         subtitle_size = 13,
+                         subtitle_margin = 10,
+                         plot_title_size = 18,
+                         plot_title_margin = 10,
+                         ...) {
     ret <- ggplot2::theme_minimal(base_family = "RobotoCondensed-Regular",
                                   base_size = base_size, ...)
-    ret$plot.title <- ggplot2::element_text(family="Roboto-Bold",
-                                            size = rel(1.2),
-                                            hjust = 0, vjust = 1,
-                                            margin = ggplot2::margin(b = base_size / 2 * 1.2)
-                                            )
+    ret$strip.text <- ggplot2::element_text(hjust = 0, size=strip_text_size,
+                                            margin=margin(b=strip_text_margin),
+                                            family="Roboto-Bold")
+    ret$plot.subtitle <- ggplot2::element_text(hjust = 0, size=subtitle_size,
+                                               margin=margin(b=subtitle_margin),
+                                               family="RobotoCondensed-Regular")
+    ret$plot.title <- ggplot2::element_text(hjust = 0, size = plot_title_size,
+                                            margin=margin(b=plot_title_margin),
+                                            family="Roboto-Bold")
     ret
 }
+
+
+
+
+#' Minimal ggplot2 theme using the IBM Plex Sans fonts
+#'
+#' @param base_size base font size
+#' @param ... Other arguments passed to \code{theme_minimal}
+#'
+#' @details The IBM Plex fonts are open sources and can be found at
+#' \url{https://ibm.github.io/type/}. These fonts must be installed locally on
+#' your computer for this theme to work.
+#'
+#' @examples
+#' \dontrun{
+#' library(ggplot2)
+#'
+#' ggplot(mtcars, aes(wt, mpg)) +
+#'     geom_point() +
+#'     labs(title = "A Lovely Plot",
+#'          subtitle = "What can the subtitle tell us?") +
+#'     theme_plex()
+#'
+#' ggplot(diamonds, aes(carat, price, color = clarity)) +
+#'     geom_point() +
+#'     facet_wrap(~cut) +
+#'     labs(title = "A Lovely Plot",
+#'          subtitle = "What can the subtitle tell us?") +
+#'          theme_plex()
+#'
+#'}
+#'
+#' @export
+theme_plex <- function(base_size = 11,
+                       strip_text_size = 12,
+                       strip_text_margin = 5,
+                       subtitle_size = 13,
+                       subtitle_margin = 10,
+                       plot_title_size = 18,
+                       plot_title_margin = 10,
+                       ...) {
+    ret <- ggplot2::theme_minimal(base_family = "IBMPlexSans",
+                                  base_size = base_size, ...)
+    ret$strip.text <- ggplot2::element_text(hjust = 0, size=strip_text_size,
+                                            margin=margin(b=strip_text_margin),
+                                            family="IBMPlexSans-Medium")
+    ret$plot.subtitle <- ggplot2::element_text(hjust = 0, size=subtitle_size,
+                                               margin=margin(b=subtitle_margin),
+                                               family="IBMPlexSans")
+    ret$plot.title <- ggplot2::element_text(hjust = 0, size = plot_title_size,
+                                            margin=margin(b=plot_title_margin),
+                                            family="IBMPlexSans-Bold")
+    ret
+}
+
