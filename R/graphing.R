3e04f644da0dd0dea7c43a38834ce9131b7b9cd3
Julia Silge <julia.silge@gmail.com>
2022-10-19 15:39:48 -0600

    Add a light theme, FINALLY

--- R/graphing.R
+++ R/graphing.R
@@ -114,3 +114,63 @@ theme_plex <- function(base_size = 11,
     ret
 }
 
+#' Light ggplot2 theme using the IBM Plex Sans fonts
+#'
+#' @param base_size base font size
+#' @param strip_text_size,strip_text_margin plot strip text size and margin
+#' @param subtitle_size,subtitle_margin plot subtitle size and margin
+#' @param plot_title_size,plot_title_margin plot title size and margin
+#' @param ... Other arguments passed to \code{theme_light}
+#'
+#' @details The IBM Plex fonts are open source and can be found at
+#' \url{https://ibm.github.io/type/}. These fonts must be installed locally on
+#' your computer for this theme to work.
+#'
+#' @examples
+#' \dontrun{
+#' library(ggplot2)
+#'
+#' ggplot(mtcars, aes(wt, mpg)) +
+#'     geom_point() +
+#'     labs(title = "A Lovely Plot",
+#'          subtitle = "What can the subtitle tell us?") +
+#'     theme_light_plex()
+#'
+#' ggplot(diamonds, aes(carat, price, color = clarity)) +
+#'     geom_point(alpha = 0.7) +
+#'     facet_wrap(~cut) +
+#'     labs(title = "A Lovely Plot",
+#'          subtitle = "What can the subtitle tell us?") +
+#'          theme_light_plex()
+#'
+#'}
+#'
+#' @export
+theme_light_plex <- function(base_size = 11,
+                             strip_text_size = 12,
+                             strip_text_margin = 5,
+                             subtitle_size = 13,
+                             subtitle_margin = 10,
+                             plot_title_size = 16,
+                             plot_title_margin = 10,
+                             ...) {
+    ret <- ggplot2::theme_light(base_family = "IBMPlexSans",
+                                base_size = base_size, ...)
+    ret$strip.text <- ggplot2::element_text(
+        colour = "white",
+        size = strip_text_size,
+        margin = ggplot2::margin(b = strip_text_margin, t = strip_text_margin),
+        family = "IBMPlexSans-Medium"
+    )
+    ret$plot.subtitle <- ggplot2::element_text(
+        hjust = 0, size = subtitle_size,
+        margin = ggplot2::margin(b = subtitle_margin),
+        family = "IBMPlexSans"
+    )
+    ret$plot.title <- ggplot2::element_text(
+        hjust = 0, size = plot_title_size,
+        margin = ggplot2::margin(b = plot_title_margin),
+        family = "IBMPlexSans-Bold"
+    )
+    ret
+}
